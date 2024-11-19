a956768244a2cac430274fa34f1f5333f6f807fc
Genbuliu <15019822050genbu@gmail.com>
2024-11-19 10:48:14 +0800

    Update config.js

--- themes/medium/config.js
+++ themes/medium/config.js
@@ -5,17 +5,17 @@ const CONFIG = {
 
   MEDIUM_POST_LIST_COVER: true, // 文章列表显示图片封面
   MEDIUM_POST_LIST_PREVIEW: true, // 列表显示文章预览
-  MEDIUM_POST_LIST_CATEGORY: true, // 列表显示文章分类
-  MEDIUM_POST_LIST_TAG: true, // 列表显示文章标签
+ // MEDIUM_POST_LIST_CATEGORY: true, // 列表显示文章分类
+ // MEDIUM_POST_LIST_TAG: true, // 列表显示文章标签
 
   MEDIUM_POST_DETAIL_CATEGORY: true, // 文章显示分类
   MEDIUM_POST_DETAIL_TAG: true, // 文章显示标签
 
   // 菜单
-  MEDIUM_MENU_CATEGORY: true, // 显示分类
-  MEDIUM_MENU_TAG: true, // 显示标签
-  MEDIUM_MENU_ARCHIVE: true, // 显示归档
-  MEDIUM_MENU_SEARCH: true, // 显示搜索
+  //MEDIUM_MENU_CATEGORY: true, // 显示分类
+  //MEDIUM_MENU_TAG: true, // 显示标签
+ // MEDIUM_MENU_ARCHIVE: true, // 显示归档
+  //MEDIUM_MENU_SEARCH: true, // 显示搜索
 
   // Widget
   MEDIUM_WIDGET_REVOLVER_MAPS: process.env.NEXT_PUBLIC_WIDGET_REVOLVER_MAPS || 'false', // 地图插件
